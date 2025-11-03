ae083d140a31677cfa90c2946f0ac309e025b848
Vladimir <sklyrenkovssv@gmail.com>
2025-11-03 22:39:50 +0200

    update

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import './modules/titlemain.js'
 
 import AOS from "aos";
 import "aos/dist/aos.css";
-AOS.init({duration: 2000, once: true,});
+AOS.init({duration: 2000, once: false,});
 
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
